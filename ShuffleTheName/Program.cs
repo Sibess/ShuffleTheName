65c76a0bbe7ee0963a4a9131c246e867f44a7f50
Siarhei <taler34@gmail.com>
2021-06-03 14:48:55 +0300

    shuffle the names

---
+++ ShuffleTheName/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+
+//namespace My_Awesome_Program
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine(NameShuffle("Donald Trump"));
+//        }
+
+//        public static string NameShuffle(string str)
+//        {
+//            string[] fullname = str.Split(" ");
+//            string reversedName = string.Join(" ", fullname.Reverse());
+//            return reversedName;
+//        }
+//    }
+//}
+
+
+namespace My_Awesome_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NameShuffle("Donald Trump"));
+        }
+
+        public static string NameShuffle(string str)
+        {
+            string[] fullname = str.Split(" ");
+            string firstname = fullname.First();
+            string lastname = fullname.Last();
+
+            string reversedName = lastname + " " + firstname;
+            return reversedName;
+        }
+    }
+}
